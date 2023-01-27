30672d5b120ba876d4a5ae81062935d93bd8f451
nHellstrom <nikhellstrom@hotmail.com>
2023-01-27 13:57:25 +0100

    User secreted

--- Program.cs
+++ Program.cs
@@ -2,14 +2,11 @@ using milk_backend.Models;
 using milk_backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var ConnectionURI = builder.Configuration["MongoDB:ConnectionURI"];
 
 builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
 builder.Services.AddSingleton<MongoDBService>();
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -26,7 +23,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -34,11 +30,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors();
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
